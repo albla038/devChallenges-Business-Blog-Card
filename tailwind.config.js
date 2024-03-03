fb8f2605f3a190107d0d5955fb4361da28f7b670
Albert Lännerström <albert.lannerstrom@gmail.com>
2024-03-03 20:10:59 +0100

    Theme setup

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,19 @@
 module.exports = {
   content: ["./**/*.html"],
   theme: {
-    extend: {},
+    colors: {
+      white: "#FFFFFF",
+      gray: {
+        light: "#F2F5F9",
+        medium: "#677489",
+        dark: "#111729",
+      },
+    },
+    extend: {
+      fontFamily: {
+        sans: ['"Lato"', "ui-sans-serif", "system-ui"],
+      },
+    },
   },
   plugins: [],
 };
